f8488b6208a9ca583488b5b80c193508929844d7
himanushi <himanushi@users.noreply.github.com>
2025-02-21 22:51:25 +0900

    fix

--- test/setup.ts
+++ test/setup.ts
@@ -1,14 +1,8 @@
 import "@testing-library/jest-dom";
 import * as matchers from "@testing-library/jest-dom/matchers";
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, type expect } from "vitest";
+import { afterEach } from "vitest";
 import { expect as vitestExpect } from "vitest";
-
-declare module "vitest" {
-  interface Assertion<T = any>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
-}
 
 vitestExpect.extend(matchers);
 
